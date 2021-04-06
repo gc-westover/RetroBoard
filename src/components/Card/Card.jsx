89d4bb4e8b962e1ce12ec32d3dbda85fbfb8460b
Greg W <gcwestover@gmail.com>
2021-04-06 10:09:55 -0400

    Added icon SVGs as image elements

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,7 +1,12 @@
-// import {} from "react";
+import Delete from "../../assets/Icons/trash-alt-regular.svg";
+import Left from "../../assets/Icons/caret-left-solid.svg";
+import Right from "../../assets/Icons/caret-right-solid.svg";
+import Like from "../../assets/Icons/thumbs-up-regular.svg";
+import Dislike from "../../assets/Icons/thumbs-down-regular.svg";
+
+const iconSize = "20"
 
 const Card = (props) => {
-
   return (
     <div class="RetroCard" aria-label="Retro card">
       <textarea
@@ -10,27 +15,52 @@ const Card = (props) => {
         aria-label="Enter text here"
         rows="1"
         value={props.content}
-        onChange={(e)=>props.update(e, props.id, "content")}
+        onChange={(e) => props.update(e, props.id, "content")}
       ></textarea>
 
       <div class="button-group">
-        <button type="button" class="button button-left" title="Move left" onClick={()=>props.moveCard(props.id,'left')}>
-          <img src="angleLeft.svg" alt="Move left" width="12" height="12" />
+        <button
+          type="button"
+          class="button button-left"
+          title="Move left"
+          onClick={() => props.moveCard(props.id, "left")}
+        >
+          <img src={Left} alt="Move left" width={iconSize} height={iconSize}/>
         </button>
-        <button type="button" class="button button-delete" title="Delete" onClick={() => props.removeCard(props.id)}>
-          <img src="timesCircle.svg" alt="Delete" width="12" height="12" />
+        <button
+          type="button"
+          class="button button-delete"
+          title="Delete"
+          onClick={() => props.removeCard(props.id)}
+        >
+          <img src={Delete} alt="Delete" width={iconSize} height={iconSize} />
         </button>
         <div>
-          <button type="button" class="button button-left" title="Like" onClick={(e)=>props.update(e,props.id,"likes")}>
-            <img src="thumbsUp.svg" alt="Like" width="12" height="12" />
+          <button
+            type="button"
+            class="button button-left"
+            title="Like"
+            onClick={(e) => props.update(e, props.id, "likes")}
+          >
+            <img src={Like} alt="Like" width={iconSize} height={iconSize} />
           </button>
           {props.likes}
-          <button type="button" class="button button-left" title="Dislike" onClick={(e)=>props.update(e,props.id, "dislikes")}>
-            <img src="thumbsDown.svg" alt="Dislike" width="12" height="12" />
+          <button
+            type="button"
+            class="button button-left"
+            title="Dislike"
+            onClick={(e) => props.update(e, props.id, "dislikes")}
+          >
+            <img src={Dislike} alt="Dislike" width={iconSize} height={iconSize} />
           </button>
           {props.dislikes}
-          <button type="button" class="button button-right" title="Move right" onClick={()=>props.moveCard(props.id,'right')}>
-            <img src="angleRight.svg" alt="Move right" width="12" height="12" />
+          <button
+            type="button"
+            class="button button-right"
+            title="Move right"
+            onClick={() => props.moveCard(props.id, "right")}
+          >
+            <img src={Right} alt="Move right" width={iconSize} height={iconSize} />
           </button>
         </div>
       </div>
